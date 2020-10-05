09e5c66058fa5d4d4f589caaa67472ea837f2f05
Oliver Janka <>
2020-10-05 17:11:17 +0200

    Changed the website design

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
         <title>Thememaker</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/styles.css">
-        <link rel="icon" href="img/BD.png">
+        <link rel="icon" href="img/BD_transparent.png">
     </head>
     <body>
         <div id="wrapper"></div>
+            
+            <div id="bannerspace">
+                <center><img id="banner" src="img/headerbg_tight.png" alt="banner" draggable=false></center>
+                <img id="logo"src="img/BD_transparent.png" class="logo" alt="ICON BD" draggable=false>
+            </div>
+
             <header>
                 <div>
-                    <img src="img/BD.png" class="logo" alt="ICON BD" draggable=false>
                     <nav>
                         <ul>
                             <li class="cp">Home</li>
@@ -20,14 +25,14 @@
                 </div>
             </header>
             <main>
-                
+             <!--   <img src="img/bgbackground.png" alt="background.png"> -->
             </main>
-            <footer>
+            <!--<footer>
                 <div class="left">
                 </div>
                 <div class="right">
                 </div>
-            </footer>
+            </footer> -->
         </div>
     </body>
 </html>
