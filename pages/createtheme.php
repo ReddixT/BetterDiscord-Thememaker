89d6d5485d487334082823118e01e29575b42069
Oliver Janka <>
2020-10-05 14:07:31 +0200

    Added Webserver support

--- pages/createtheme.php
+++ pages/createtheme.php
@@ -3,24 +3,24 @@
     <head>
         <title>Thememaker</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/styles.css">
-        <link rel="icon" href="img/BD.png">
+        <link rel="stylesheet" href="../css/styles.css">
+        <link rel="icon" href="../img/BD.png">
     </head>
     <body>
         <div id="wrapper"></div>
             <header>
                 <div>
-                    <img src="img/BD.png" class="logo" alt="ICON BD">
+                    <img src="../img/BD.png" class="logo" alt="ICON BD">
                     <nav>
                         <ul>
-                            <li class="cp">Home</li>
-                            <li><a href="pages/createtheme.html">Create a Theme</a></li>
+                            <li><a href="../index.php">Home</a></li>
+                            <li class="cp">Create a Theme</li>
                         </ul>
                     </nav>
                 </div>
             </header>
             <main>
-                
+
             </main>
             <footer>
                 <div class="left">
