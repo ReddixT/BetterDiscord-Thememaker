7987606d105b80c34d32769e300942f2c5192d82
René Ottenburg <reneottenburg@icloud.com>
2020-10-07 15:38:14 +0200

    added new page, updated bd download link

--- pages/createtheme.php
+++ pages/createtheme.php
@@ -48,8 +48,9 @@ if ($_POST['generated']) {
                     <ul>
                         <a href="../index.php" draggable=false><li>Home</li></a>
                         <li class="cp">Create a Theme</li>
-                        <a href="../pages/clearvision.php" draggable=false><li>ClearVision</li><a>
-                        <a href="https://betterdiscord.net/home/" draggable=false><li>Download BD</li></a>
+                        <a href="../pages/clearvision.php"><li>ClearVision</li><a>
+                        <a href="../pages/library.php"><li>Library</li></a>
+                        <a href="https://github-production-release-asset-2e65be.s3.amazonaws.com/155161153/c7d2fb00-d1b5-11ea-99e8-f5ca60f47502?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20201007%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201007T133112Z&X-Amz-Expires=300&X-Amz-Signature=e578fa6ab8b7b286918c8629240895289558e2c80700400a082f79e3a1187c62&X-Amz-SignedHeaders=host&actor_id=63563893&key_id=0&repo_id=155161153&response-content-disposition=attachment%3B%20filename%3DBandagedBD.exe&response-content-type=application%2Foctet-stream"><li>Download BD</li></a>
                     </ul>
                 </nav>
             </div>
---
+++ pages/library.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ClearVision Credits</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/classes.css">
+    <link rel="stylesheet" href="../css/ids.css">
+    <link rel="icon" href="../img/BD.png">
+</head>
+
+<body>
+    <div id="wrapper"></div>
+    <header>
+        <div id="bannerspace">
+            <center><img id="banner" src="../img/headerbg_tight_transparent.png" alt="banner" draggable=false></center>
+            <img id="logo" src="../img/BD_transparent.png" class="logo" alt="ICON BD" draggable=false>
+        </div>
+
+        <header>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="../index.php">Home</a></li>
+                        <li><a href="../pages/createtheme.php">Create a Theme</a></li>
+                        <li><a href="../pages/clearvision.php">ClearVision</a></li>
+                        <li class="cp">Library</li>
+                        <a href="https://github-production-release-asset-2e65be.s3.amazonaws.com/155161153/c7d2fb00-d1b5-11ea-99e8-f5ca60f47502?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20201007%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201007T133112Z&X-Amz-Expires=300&X-Amz-Signature=e578fa6ab8b7b286918c8629240895289558e2c80700400a082f79e3a1187c62&X-Amz-SignedHeaders=host&actor_id=63563893&key_id=0&repo_id=155161153&response-content-disposition=attachment%3B%20filename%3DBandagedBD.exe&response-content-type=application%2Foctet-stream"><li>Download BD</li></a>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+            <div class="left">
+
+            </div>
+
+            <div class="right">
+
+            </div>
+        </footer>
+        </div>
+</body>
+
+</html>
