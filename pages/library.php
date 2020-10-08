81bf637f99751fbf2ce308010e8782e8c348329f
René Ottenburg <reneottenburg@icloud.com>
2020-10-08 10:29:35 +0200

    fix website

--- pages/library.php
+++ pages/library.php
@@ -13,7 +13,6 @@
 <body>
     <div id="wrapper"></div>
     <div id="bannerspace">
-<<<<<<< HEAD
         <center><img id="banner" src="../img/headerbg_tight_transparent.png" alt="banner" draggable=false></center>
         <img id="logo" src="../img/BD_transparent.png" class="logo" alt="ICON BD" draggable=false>
     </div>
@@ -41,22 +40,6 @@
                     </a>
                 </p>
                 <div class="pictext">BasicBackground By: DevilBro</div>
-=======
-            <center><img id="banner" src="../img/headerbg_tight_transparent.png" alt="banner" draggable=false></center>
-            <a href="../index.php"><img id="logo" src="../img/BD_transparent.png" class="logo" alt="ICON BD" draggable=false></a>
-        </div>
-        <header>
-            <div>
-                <nav>
-                    <ul>
-                        <a href="../index.php" draggable=false><li>Home</li></a>
-                        <a href="../pages/createtheme.php"><li>Create a Theme</li></a>
-                        <a href="../pages/clearvision.php"><li>ClearVision</li>
-                        <li class="cp">Library</li>
-                        <a href="https://github.com/rauenzi/BBDInstaller/releases/latest/download/BandagedBD.exe"><li>Download BD</li></a>
-                    </ul>
-                </nav>
->>>>>>> de42fa9... Library fix
             </div>
             <div class="column4">
                 <p style="text-align:center;">
@@ -110,7 +93,6 @@
                 </p>
                 <div class="pictext">SimplyDark By: Gibbu</div>
             </div>
-<<<<<<< HEAD
             <div class="column4">
                 <p style="text-align:center;">
                     <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/CapnKitten/Translucence/master/Translucence.theme.css" download>
@@ -139,9 +121,6 @@
             <a href="https://www.zli.ch/">ZLI</a>
         </div>
     </footer>
-=======
-        </footer>
->>>>>>> de42fa9... Library fix
     </div>
 </body>
 
