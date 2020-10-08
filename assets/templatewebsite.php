09c365a2885eccea81f555743a3951d3e3c59ac7
René Ottenburg <reneottenburg@icloud.com>
2020-10-08 09:45:10 +0200

    template fixed

--- assets/templatewebsite.php
+++ assets/templatewebsite.php
@@ -12,37 +12,38 @@
 
 <body>
     <div id="wrapper"></div>
+    <div id="bannerspace">
+        <center><img id="banner" src="../img/headerbg_tight_transparent.png" alt="banner" draggable=false></center>
+        <img id="logo" src="../img/BD_transparent.png" class="logo" alt="ICON BD" draggable=false>
+    </div>
     <header>
-        <div id="bannerspace">
-            <center><img id="banner" src="../img/headerbg_tight.png" alt="banner" draggable=false></center>
-            <img id="logo" src="../img/BD_transparent.png" class="logo" alt="ICON BD" draggable=false>
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="../pages/createtheme.php">Create a Theme</a></li>
+                    <li><a href="../pages/clearvision.php">ClearVision</a></li>
+                    <li class="cp">Library</li>
+                    <a href="https://github.com/rauenzi/BBDInstaller/releases/latest/download/BandagedBD.exe">
+                        <li>Download BD</li>
+                    </a>
+                </ul>
+            </nav>
         </div>
-        <header>
-            <div>
-                <nav>
-                    <ul>
-                        <li><a href="../index.php">Home</a></li>
-                        <li><a href="../pages/createtheme.php">Create a Theme</a></li>
-                        <li><a href="../pages/clearvision.php">ClearVision</a></li>
-                        <a href="../pages/library.php"><li>Library</li></a>
-                        <a href="https://github.com/rauenzi/BBDInstaller/releases/latest/download/BandagedBD.exe"><li>Download BD</li></a>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main>
+    </header>
+    <main>
 
-        </main>
-        <footer>
-            <div class="left">
+    </main>
+    <footer>
+        <div class="left">
 
-            </div>
-
-            <div class="right">
-
-            </div>
-        </footer>
         </div>
+
+        <div class="right">
+
+        </div>
+    </footer>
+    </div>
 </body>
 
 </html>
