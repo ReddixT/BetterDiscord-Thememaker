766fa507b9b4c467b86b0fa6ccc73d6947a0a066
Robin Lehmann <72382720+Sorry4Nothing@users.noreply.github.com>
2020-10-08 13:43:13 +0200

    tutorial created

---
+++ pages/tutorial.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ThemeMaker Library</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/classes.css">
+    <link rel="stylesheet" href="../css/ids.css">
+    <link rel="icon" href="../img/BD.png">
+</head>
+
+
+<footer>
+    <div class="left">
+        <p>Project Contributors: <br>René, Matteo, Robin, Oliver<br></p>
+    </div>
+
+    <div class="right">
+        <a href="https://github.com/ReddixT/BetterDiscord-Thememaker" draggable=false>GitHub<br></a>
+        <a href="https://www.zli.ch/" draggable=false>ZLI</a>
+    </div>
+</footer>
+
+</html>
