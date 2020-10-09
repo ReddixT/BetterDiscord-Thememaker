7883c0ce57038ecba4de1de8430d8a3d1c65d6ce
Robin Lehmann <72382720+Sorry4Nothing@users.noreply.github.com>
2020-10-09 14:45:22 +0200

    icon fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/classes.css">
     <link rel="stylesheet" href="css/ids.css">
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="icon" href="../img/BD.png">
+    <link rel="icon" href="img/BD.png">
 </head>
 
 <body>
@@ -92,7 +92,7 @@
         </div>
 
         <div class="right">
-            <a id="projectlogo" draggable="false"><img src="img/projectlogo.png" draggable="false"></a>
+            <a id="projectlogo" draggable="false"><img src="img/BD_transparent2.png" draggable="false"></a>
             <a id="discordlogo" href="https://discord.com/" draggable="false"><img src="img/discord_icon.png" draggable="false"></a>
             <a id="waage" href="https://github.com/ReddixT/BetterDiscord-Thememaker/blob/main/LICENSE" draggable="false"><img src="img/waage.png" draggable="false"></a>
             <a id="githublogo" href="https://github.com/ReddixT/BetterDiscord-Thememaker" draggable="false"><img src="img/github.png" draggable=false></a>
