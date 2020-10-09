79a06ad577c6680f4eca2fdf1f39c54735d58979
Robin Lehmann <72382720+Sorry4Nothing@users.noreply.github.com>
2020-10-09 13:15:11 +0200

    label implement

--- index.php
+++ index.php
@@ -92,6 +92,7 @@
         </div>
 
         <div class="right">
+            <a id="projectlogo" draggable="false"><img src="img/projectlogo.png" draggable="false"></a>
             <a id="discordlogo" href="https://discord.com/" draggable="false"><img src="img/discord_icon.png" draggable="false"></a>
             <a id="waage" href="https://github.com/ReddixT/BetterDiscord-Thememaker/blob/main/LICENSE" draggable="false"><img src="img/waage.png" draggable="false"></a>
             <a id="githublogo" href="https://github.com/ReddixT/BetterDiscord-Thememaker" draggable="false"><img src="img/github.png" draggable=false></a>
